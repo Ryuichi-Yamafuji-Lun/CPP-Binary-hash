867a1c1854a3f719d26cae13916b8e127da7391a
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2022-10-11 11:12:59 +0900

    Pushing binary and hash file

---
+++ binary.cpp
@@ -0,0 +1,38 @@
+//binary search implementation
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int binary_search(int array[],int n,int num){
+    int left = 0;
+    int right = n - 1;
+    //search through array
+    while(left <= right){
+        //find mid length
+        int mid = (left + right)/2;
+        if(array[mid] < num){
+            left = mid + 1;
+        }
+        else if(array[mid] > num){
+            right = mid - 1;
+        }
+        else{
+            return mid;
+        }
+    }
+    return -1;
+}
+int main(void){
+    //array
+    int array[] = {1,2,3,4,5,6,7,8,9,10};
+    int num = 10;
+    int n = *(&array + 1) - array;
+    int result = binary_search(array, n, num);
+    if(result == -1) {
+        cout << "element is not present";
+    }
+    else{
+        cout << "element is present at index: " << result;
+    }
+    return 0;
+}
